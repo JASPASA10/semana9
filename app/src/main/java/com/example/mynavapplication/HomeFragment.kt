44fef6887f39ddb1920d8ee7a677510d8c96b457
Crid <contact@crid.com>
2025-10-01 21:05:52 -0500

    Initial commit - Actividad_Semana_9

---
+++ app/src/main/java/com/example/mynavapplication/HomeFragment.kt
@@ -0,0 +1,19 @@
+package com.example.mynavapplication
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentContainer
+
+class HomeFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+}
